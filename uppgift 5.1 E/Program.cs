4398913537d3678637bf91eddfe5e650d79c640b
en301 <emilsjostrand1000@gmail.com>
2023-11-22 09:53:41 +0100

    Add project files.

---
+++ uppgift 5.1 E/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace upggift_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 5 namn");
+
+            string[] namn = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                namn[i] = Console.ReadLine();
+            }
+            for (int j = 0;j  < 5;j++)
+            {
+                Console.WriteLine(namn[j]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
